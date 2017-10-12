573372516dca6ced7f153cc9485fa577d6c7efa6
picongzhi <picongzhi@gmail.com>
2017-10-12 21:50:24 +0800

    add use_pil_resize.py

---
+++ pil/use_pil_resize.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+
+im = Image.open('cheer.jpg')
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+im.thumbnail((w // 2, h // 2))
+print('Resize image to: %sx%s' % (w // 2, h // 2))
+im.save('thumbnail.jpg', 'jpeg')
